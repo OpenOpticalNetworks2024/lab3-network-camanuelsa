8697879158fb47c1608d90ccdcb75b581f993123
Manuel Cataldo <manuelcataldo51@gmail.com>
2024-10-23 18:43:51 +0200

    Modified some of the first three classes and added the last one along with the main file

--- core/elements.py
+++ core/elements.py
@@ -1,10 +1,12 @@
 import json
-from parameters import cf
+import matplotlib.pyplot as plt
+import math
+from core.parameters import cf
 
 class Signal_information(object):
     def __init__(self, signal_power, path):
         self._signal_power = signal_power
-        self._noise_power = 0.0
+        self._noise_power = 1e-12
         self._latency = 0.0
         self._path = path
 
@@ -52,9 +54,9 @@ class Signal_information(object):
 
 class Node(object):
     def __init__(self, label, position, connected_nodes):
-        self._label = str(label)
-        self._position = tuple(position)
-        self._connected_nodes = list(map(str, connected_nodes))
+        self._label = label
+        self._position = position
+        self._connected_nodes = connected_nodes
         self._successive = {}
 
     @property
@@ -81,14 +83,15 @@ class Node(object):
         sgn.update_path()
         if sgn.path:
             nxt = sgn.path[0]
-            if nxt in self._successive:
-                self._successive[nxt].propagate(sgn)
+            line_label = self.label + nxt
+            if line_label in self._successive:
+                self._successive[line_label].propagate(sgn)
 
 
 class Line(object):
     def __init__(self, label, length):
-        self._label = str(label)
-        self._length = float(length)
+        self._label = label
+        self._length = length
         self._successive = {}
 
     @property
@@ -122,31 +125,83 @@ class Line(object):
                 self._successive[nxt].propagate(sgn)
 
 class Network(object):
-    def __init__(self):
-        pass
+    def __init__(self, json_file):
+        self._nodes = {}
+        self._lines = {}
+
+        file = open(json_file, 'r')
+        data = json.load(file)
+        file.close()
+
+        for label, node_data in data.items():
+            node = Node(label, tuple(node_data['position']), node_data['connected_nodes'])
+            self._nodes[label] = node
+
+        for node_label, node in self._nodes.items():
+            for connected_node_label in node.connected_nodes:
+                if connected_node_label in self._nodes:
+                    connected_node = self._nodes[connected_node_label]
+                    distance = math.sqrt((node.position[0] - connected_node.position[0]) ** 2 + (node.position[1] - connected_node.position[1]) ** 2)
+                    line_label = node_label + connected_node_label
+                    self._lines[line_label] = Line(line_label, distance)
 
     @property
     def nodes(self):
-        pass
+        return self._nodes
 
     @property
     def lines(self):
-        pass
+        return self._lines
 
     def draw(self):
-        pass
+        fig, ax = plt.subplots()
+        for line in self._lines.values():
+            node_labels = line.label
+            x_values = [self._nodes[node_labels[0]].position[0], self._nodes[node_labels[1]].position[0]]
+            y_values = [self._nodes[node_labels[0]].position[1], self._nodes[node_labels[1]].position[1]]
+            ax.plot(x_values, y_values, 'go-')
+
+        for node in self._nodes.values():
+            ax.text(node.position[0], node.position[1], node.label, fontsize=24)
+
+        plt.xlabel('X position')
+        plt.ylabel('Y position')
+        plt.title('Network')
+        plt.grid(True)
+        plt.savefig('network.png')
+        plt.show()
 
     # find_paths: given two node labels, returns all paths that connect the 2 nodes
     # as a list of node labels. Admissible path only if cross any node at most once
-    def find_paths(self, label1, label2):
-        pass
+    def find_paths(self, label1, label2, path=None):
+        if path is None:
+            path = []
+        path = path + [label1]
+        if label1 == label2:
+            return [path]
+        if label1 not in self._nodes:
+            return []
+        paths = []
+        for node_label in self._nodes[label1].connected_nodes:
+            if node_label not in path:
+                new_paths = self.find_paths(node_label, label2, path)
+                for new_path in new_paths:
+                    paths.append(new_path)
+        return paths
 
     # connect function set the successive attributes of all NEs as dicts
     # each node must have dict of lines and viceversa
     def connect(self):
-        pass
+        for node_label, node in self._nodes.items():
+            for connected_node_label in node.connected_nodes:
+                if connected_node_label in self._nodes:
+                    line_label = node_label + connected_node_label
+                    node.successive[line_label] = self._lines[line_label]
+                    self._lines[line_label].successive[connected_node_label] = self._nodes[connected_node_label]
 
     # propagate signal_information through path specified in it
     # and returns the modified spectral information
     def propagate(self, signal_information):
-        pass
+        start_node_label = signal_information.path[0]
+        self._nodes[start_node_label].propagate(signal_information)
+        return signal_information
--- tasks/lab3_network_main.py
+++ tasks/lab3_network_main.py
@@ -1,14 +1,37 @@
-import json
-import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 from pathlib import Path
+from core.math_utils import *
+from core.elements import *
 
 # Exercise Lab3: Network
 
 ROOT = Path(__file__).parent.parent
 INPUT_FOLDER = ROOT / 'resources'
 file_input = INPUT_FOLDER / 'nodes.json'
+network = Network(file_input)
+network.connect()
+paths_data = []
+for src in network.nodes:
+    for dst in network.nodes:
+        if src != dst:
+            paths = network.find_paths(src, dst)
+            for path in paths:
+                path_string = '->'.join(path)
+                signal = network.propagate(Signal_information(1e-3, path))
+                total_latency = signal.latency
+                total_noise = signal.noise_power
+                snr_db = lin2db(signal.signal_power / total_noise)
+                paths_data.append({
+                    'Path': path_string,
+                    'Total Latency (s)': total_latency,
+                    'Total Noise (W)': total_noise,
+                    'SNR (dB)': snr_db
+                })
+df = pd.DataFrame(paths_data)
+csv_path = ROOT / 'weighted_path.csv'
+df.to_csv(csv_path, index=False)
+network.draw()
+print(f"Results saved to {csv_path}")
 
 
 # Load the Network from the JSON file, connect nodes and lines in Network.
